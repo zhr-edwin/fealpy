21bea439ebf6524ab2e3182c5d2fcc1789e8f864
GaoTingyi <97591315+gaotingyi@users.noreply.github.com>
2025-09-23 09:03:48 +0800

    feat(cgraph):增加带空腔的球体网格 (#1888)
    * feat(cgraph): update load and dump to support subgraph and node group
    
    * refactor(cgraph): rename gi/go to gin/gout.
    
    * feat(cgraph): no error raised for CNodeType without TITLE, PATH or DESC. Add test files for search_all_nodes and search_node.
    
    * feat(cgraph): write title of nodes and slots in Chinese.
    
    * wip: replace path of nodes into Chinese.
    
    * feat(cgraph):增加带空腔的球体网格
    
    ---------
    
    Co-authored-by: Mualani <zhengyy0621@foxmail.com>
    Co-authored-by: AlbertZyy <108785827+AlbertZyy@users.noreply.github.com>

--- fealpy/cgraph/mesh/__init__.py
+++ fealpy/cgraph/mesh/__init__.py
@@ -1,3 +1,5 @@
+from .creation import Box2d
+from .creation import SphericalShell3d
 from .creation import Box2d, DLDMicrofluidicChipMesh2d, CreateMesh, Box3d, CircleMesh
 from .ops import ErrorEstimation, MeshDimensionUpgrading
 
